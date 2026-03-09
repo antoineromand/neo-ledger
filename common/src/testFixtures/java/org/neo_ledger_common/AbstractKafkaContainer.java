e108e4aaad34fa9c7655fee28102ea8b5726f887
Antoine Romand <antoine.romand@epitech.eu>
2026-03-10 00:04:31 +0100

    feat: add AbstractKafkaContainer (singleton for test only), use testFixtures to only use some dependencies on test package, update both build.gradle and add to docker compose my image from docker hub public repo.

---
+++ common/src/testFixtures/java/org/neo_ledger_common/AbstractKafkaContainer.java
@@ -0,0 +1,27 @@
+package org.neo_ledger_common;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.kafka.KafkaContainer;
+import org.testcontainers.utility.DockerImageName;
+
+public abstract class AbstractKafkaContainer {
+    protected static final KafkaContainer KAFKA;
+
+    static {
+        KAFKA = new KafkaContainer(DockerImageName.parse(("apache/kafka:3.7.0")));
+        KAFKA.start();
+        Runtime.getRuntime().addShutdownHook(new Thread(KAFKA::stop));
+    }
+
+    public static String getBootstrapServers() {
+        return KAFKA.getBootstrapServers();
+    }
+
+    @DynamicPropertySource
+    static void kafkaProperties(DynamicPropertyRegistry registry) {
+        // Cette ligne "branche" l'adresse du conteneur dynamique
+        // sur la clé attendue par Spring Boot.
+        registry.add("spring.kafka.bootstrap-servers", KAFKA::getBootstrapServers);
+    }
+}
