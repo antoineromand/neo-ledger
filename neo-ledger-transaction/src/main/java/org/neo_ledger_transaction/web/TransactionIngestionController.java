c57db4aa0b65235b1cf8b77a373effd578d33c3c
Antoine Romand <antoine.romand@epitech.eu>
2026-03-16 22:17:21 +0100

    fix: implement openApi, annotate TransactionIngestionController

--- neo-ledger-transaction/src/main/java/org/neo_ledger_transaction/web/TransactionIngestionController.java
+++ neo-ledger-transaction/src/main/java/org/neo_ledger_transaction/web/TransactionIngestionController.java
@@ -1,6 +1,12 @@
 package org.neo_ledger_transaction.web;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.neo_ledger_transaction.application.port.in.IngestionUseCasePort;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,10 +29,21 @@ public class TransactionIngestionController {
         this.ingestionUseCasePort = ingestionUseCasePort;
     }
 
-    @PostMapping()
-    public void ingestPaymentFile(@RequestBody String paymentFile) throws XMLStreamException, IOException {
+    @PostMapping(consumes = MediaType.APPLICATION_XML_VALUE)
+    @Operation(
+            summary = "Ingest payment file",
+            description = "Ingest a payment to ingest and process transactions.",
+            operationId = "ingestPaymentFile",
+            parameters = {
+                    @Parameter(name = "paymentFile", description = "A payment file in a XML format.")
+            }
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Your payment file has been ingested")
+    })
+    public ResponseEntity<String> ingestPaymentFile(@RequestBody String paymentFile) throws XMLStreamException, IOException {
         InputStream inputStream = new ByteArrayInputStream(paymentFile.getBytes(StandardCharsets.UTF_8));
-
         this.ingestionUseCasePort.executeIngestion(inputStream);
+        return ResponseEntity.ok().body("Your payment file has been ingested");
     }
 }
