1e8705913a8838bbc702c5e44a2b16ff346c0235
Antoine Romand <antoine.romand@epitech.eu>
2026-03-19 23:49:24 +0100

    feat: remove unused exception and update ingestion service unit tests (verify orchestration and run happy test with real implementations of parser and validator).

--- neo-ledger-transaction/src/main/java/org/neo_ledger_transaction/application/exceptions/MissingParserException.java
+++
@@ -1,9 +0,0 @@
-package org.neo_ledger_transaction.application.exceptions;
-
-import org.neo_ledger.common.exceptions.TechnicalException;
-
-public class MissingParserException extends TechnicalException {
-    public MissingParserException(Throwable cause) {
-        super("Parser not implemented", cause);
-    }
-}
