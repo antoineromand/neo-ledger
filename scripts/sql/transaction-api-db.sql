bd0b3eaaeaaa3882d89fd1cc327b9f63e63b9ad5
Antoine Romand <antoine.romand@epitech.eu>
2026-03-21 23:33:07 +0100

    feat: add postgres volume, and create transaction db outbox table.

---
+++ scripts/sql/transaction-api-db.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS transaction_outbox
+(
+    id             UUID PRIMARY KEY,
+    aggregate_id   VARCHAR(255) NOT NULL,         -- end to end id
+    aggregate_type VARCHAR(50)  NOT NULL,         -- RAW_SEPA_TRANSACTION
+    event_type     VARCHAR(100) NOT NULL,         -- TRANSACTION_INGESTED
+    payload        BYTEA        NOT NULL,         -- RawSepaTransaction -> protobuf
+    created_at     TIMESTAMP    NOT NULL,
+    status         VARCHAR(20) DEFAULT 'PENDING', -- PENDING, PROCESSED, FAILED
+    processed_at   TIMESTAMP,                     -- AFTER_PUSH
+    retry_count    INT         DEFAULT 0,
+    last_error     TEXT
+);
